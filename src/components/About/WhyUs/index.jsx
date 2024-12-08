216e978d0c8a9a1b8de7a3887219e5b0074a52c8
prathyUsha37 <prathyusha.aparacheruvu@gmail.com>
2024-12-08 23:25:15 +0530

    code update

--- src/components/About/WhyUs/index.jsx
+++ src/components/About/WhyUs/index.jsx
@@ -10,31 +10,35 @@ import { motion } from "framer-motion";
 const data = [
   {
     heading: 'Industry Expertise',
-    description: `With years of experience in the staffing and recruitment industry, we understand your needs.`
+    description: `With years of experience in the staffing and recruitment industry, we understand your needs.`,
+    outlook: 'green'
   },
   {
     heading: 'Tailored Solutions',
-    description: `We offer customized staffing solutions based on your specific business goals.`
+    description: `We offer customized staffing solutions based on your specific business goals.`,
+    outlook: 'orange'
   },
   {
     heading: 'Results-Driven',
-    description: `We focus on delivering measurable results with every placement, ensuring long-term success.`
+    description: `We focus on delivering measurable results with every placement, ensuring long-term success.`,
+    outlook: 'indigo',
   },
   {
     heading: 'Dedicated Support',
-    description: `From the initial consultation to the final placement, continuous support to both clients and candidates.`
+    description: `From the initial consultation to the final placement, continuous support to both clients and candidates.`,
+    outlook: 'red'
   }
 ]
 
 function Card(props) {
-  const { data: { heading, description } } = props || {};
+  const { data: { heading, description, outlook } } = props || {};
   const theme = useMantineTheme();
 
   return (
     <motion.div
       whileHover={{
         scale: [null, 1.1],
-        boxShadow: [null, `0 0 10px ${theme.colors.gray[4]}`],
+        boxShadow: [null, `0 0 10px ${theme.colors[outlook][4]}`],
       }}
       transition={{ duration: 0.3 }}
       className={classes.card}
